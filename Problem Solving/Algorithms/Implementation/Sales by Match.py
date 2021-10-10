ad72a7299eebcf116991da1fb541227725f032f0
nhannh1526 <nhan.nh1526@gmail.com>
2021-10-10 15:39:58 +0700

    Add solutions

---
+++ Problem Solving/Algorithms/Implementation/Sales by Match.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import os
+
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+
+def sockMerchant(n, ar):
+    # Write your code here
+    return sum([ar.count(colour) // 2 for colour in set(ar)])
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
