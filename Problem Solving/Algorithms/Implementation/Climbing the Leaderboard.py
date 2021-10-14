afc3fffc82b2a72d2c1e92c7b37bcd2772b62d07
nhannh1526 <nhan.nh1526@gmail.com>
2021-10-14 16:11:01 +0700

    Add solutions

---
+++ Problem Solving/Algorithms/Implementation/Climbing the Leaderboard.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import os
+
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+
+def climbingLeaderboard(ranked, player):
+    # Write your code here
+    player_rank = []
+    ranked = sorted(set(ranked), reverse=True)
+    for score in player:
+        while ranked and score >= ranked[-1]:
+            ranked.pop()
+        player_rank.append(len(ranked) + 1)
+    return player_rank
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ Problem Solving/Algorithms/Implementation/Picking Numbers.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import os
+
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+
+def pickingNumbers(a):
+    # Write your code here
+    result = 0
+    for value in set(a):
+        count = a.count(value) + a.count(value - 1)
+        if count > result:
+            result = count
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
