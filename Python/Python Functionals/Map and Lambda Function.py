be158ac7d63799a0347b96baaa7f64acd9457af2
nhannh1526 <nhan.nh1526@gmail.com>
2021-10-24 12:43:57 +0700

    Add solutions

--- Python/Python Functionals/Map and Lambda Function.py
+++ Python/Python Functionals/Map and Lambda Function.py
@@ -1,11 +1,11 @@
-def cube(x): return x**3  # complete the lambda function
+def cube(x): return x ** 3  # complete the lambda function
 
 
 def fibonacci(n):
     # return a list of fibonacci numbers
     fibonacci_numbers = [0, 1]
     for i in range(2, n):
-        fibonacci_numbers.append(fibonacci_numbers[i-1]+fibonacci_numbers[i-2])
+        fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2])
     return fibonacci_numbers[0:n]
 
 
