58bc0ab2bea8bb8d563f71f60961fa5b4a81e619
nhannh1526 <nhan.nh1526@gmail.com>
2021-10-24 17:06:22 +0700

    Add solutions

---
+++ Problem Solving/Algorithms/Implementation/Jumping on the Clouds Revisited.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+
+
+def jumpingOnClouds(c, k):
+    n = len(c)
+    e = 100
+    i = k % n
+    e -= c[i] * 2 + 1
+    while i != 0:
+        i = (i + k) % n
+        e -= c[i] * 2 + 1
+    return e
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
