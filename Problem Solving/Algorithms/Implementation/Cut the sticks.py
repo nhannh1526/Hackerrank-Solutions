514add38897adbec711cbbf86f402ad952352932
nhannh1526 <nhan.nh1526@gmail.com>
2021-11-05 13:02:28 +0700

    Add solutions

---
+++ Problem Solving/Algorithms/Implementation/Cut the sticks.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def cutTheSticks(arr):
+    # Write your code here
+    result = []
+    while arr:
+        result.append(len(arr))
+        shortest_stick_length = min(arr)
+        arr = [x for x in arr if x != shortest_stick_length]
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
