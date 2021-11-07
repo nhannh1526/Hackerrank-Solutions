495e8ccb18bc13200587060194ff1e4b22a84c87
nhannh1526 <nhan.nh1526@gmail.com>
2021-11-07 14:26:01 +0700

    Add solutions

---
+++ Python/Regex and Parsing/Detect HTML Tags, Attributes and Attribute Values.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        [print('-> {} > {}'.format(*attr)) for attr in attrs]
+
+
+parser = MyHTMLParser()
+for _ in range(int(input())):
+    parser.feed(input())
