1034934942203ae60e99cc1062a4fd8ad442b4ee
nhannh1526 <nhan.nh1526@gmail.com>
2021-11-09 15:59:17 +0700

    Add solutions

---
+++ Problem Solving/Algorithms/Implementation/Non-Divisible Subset.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'nonDivisibleSubset' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY s
+#
+
+
+def nonDivisibleSubset(k, s):
+    # Write your code here
+    f = [0] * k
+
+    for val in s:
+        f[val % k] += 1
+
+    if k % 2 == 0:
+        f[k//2] = min(f[k//2], 1)
+
+    res = min(f[0], 1)
+
+    for i in range(1, (k//2) + 1):
+        res += max(f[i], f[k - i])
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    s = list(map(int, input().rstrip().split()))
+
+    result = nonDivisibleSubset(k, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
