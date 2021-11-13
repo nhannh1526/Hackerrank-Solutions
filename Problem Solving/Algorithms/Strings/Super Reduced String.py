0cce2b03dcf07610dcc0d21425d03b73b254b9d1
nhannh1526 <nhan.nh1526@gmail.com>
2021-11-13 23:56:23 +0700

    Add solutions

---
+++ Problem Solving/Algorithms/Strings/Super Reduced String.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+
+def superReducedString(s):
+    # Write your code here
+    for c in s:
+        if 2 * c in s:
+            s = s.replace(2 * c, "")
+    return s if s else "Empty String"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
