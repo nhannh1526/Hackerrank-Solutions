436bc4d3216c5aaaa1640587f7bd3e1fde716828
nhannh1526 <nhan.nh1526@gmail.com>
2021-11-20 01:07:27 +0700

    Add solutions

---
+++ Python/Numpy/Mean, Var, and Std.py
@@ -0,0 +1,7 @@
+import numpy
+
+n, m = map(int, input().split())
+arr = numpy.array([input().strip().split() for _ in range(n)], int)
+print(numpy.mean(arr, axis=1))
+print(numpy.var(arr, axis=0))
+print(round(numpy.std(arr), 11))
