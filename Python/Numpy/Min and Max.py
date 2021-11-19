37fc4a328232e7a1dc702b49a9325c33d6d095bb
nhannh1526 <nhan.nh1526@gmail.com>
2021-11-19 14:01:07 +0700

    Add solutions

---
+++ Python/Numpy/Min and Max.py
@@ -0,0 +1,5 @@
+import numpy
+
+n, m = map(int, input().split())
+arr = numpy.array([input().strip().split() for _ in range(n)], int)
+print(numpy.max(numpy.min(arr, axis=1)))
---
+++ Python/Numpy/Sum and Prod.py
@@ -0,0 +1,5 @@
+import numpy
+
+n, m = map(int, input().split())
+arr = numpy.array([input().strip().split() for _ in range(n)], int)
+print(numpy.prod(numpy.sum(arr, axis=0)))
